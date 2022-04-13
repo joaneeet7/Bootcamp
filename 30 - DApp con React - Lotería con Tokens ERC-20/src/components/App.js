0c3a8966b41282d10c0e2fdc8726fcb49496f939
Joan Amengual <joan@consilientlabs.com>
2022-04-13 12:57:47 -0700

    Versions

--- 30 - DApp con React - Lotería con Tokens ERC-20/src/components/App.js
+++ 30 - DApp con React - Lotería con Tokens ERC-20/src/components/App.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3'
+import smart_contract from '../abis/loteria.json'
 
 class App extends Component {
+
+  async componentWillMount() {
+    // 1. Carga de Web3
+    await this.loadWeb3()
+    // 2. Carga de datos de la Blockchain
+    await this.loadBlockchainData()
+  }
+
+  // 1. Carga de Web3
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('¡Deberías considerar usar Metamask!')
+    }
+  }
+
+  // 2. Carga de datos de la Blockchain
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    console.log('Account:', this.state.account)
+    const networkId = await web3.eth.net.getId() // Ganache -> 5777, Rinkeby -> 4, BSC -> 97
+    console.log('networkid:', networkId)
+    const networkData = smart_contract.networks[networkId]
+    console.log('NetworkData:', networkData)
+
+    if (networkData) {
+      const abi = smart_contract.abi
+      console.log('abi', abi)
+      const address = networkData.address
+      console.log('address:', address)
+      const contract = new web3.eth.Contract(abi, address)
+      this.setState({ contract })
+    } else {
+      window.alert('¡El Smart Contract no se ha desplegado en la red!')
+    }
+  }
+
+
   render() {
     return (
       <div>
@@ -37,7 +86,7 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                   ¡APRENDE BLOCKCHAIN <u><b>AHORA! </b></u>
+                  ¡APRENDE BLOCKCHAIN <u><b>AHORA! </b></u>
                 </a>
               </div>
             </main>
