1792724f4e1a1223d0152e45ed7c93b604ebdd1e
Joan Amengual <joan@consilientlabs.com>
2022-04-13 09:56:23 -0700

    Versions

--- 30 - DApp con React - Lotería con Tokens ERC-20/src/contracts/loteria.sol
+++ 30 - DApp con React - Lotería con Tokens ERC-20/src/contracts/loteria.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import "@openzeppelin/contracts@4.5.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract loteria is ERC20, Ownable {
     
