e462b721d2f130c2b17de3807f77d999bd0b7c86
Joan Amengual <joan@consilientlabs.com>
2022-04-13 00:22:33 -0700

    New

--- 30 - DApp con React - Lotería con Tokens ERC-20/src/contracts/loteria.sol
+++ 30 - DApp con React - Lotería con Tokens ERC-20/src/contracts/loteria.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
