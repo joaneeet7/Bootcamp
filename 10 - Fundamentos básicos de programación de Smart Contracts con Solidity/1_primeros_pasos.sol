7e65086e4413717e61543cce12154c885efdb77c
Joan Amengual <joan@consilientlabs.com>
2022-04-16 20:16:06 -0700

    Version

--- 10 - Fundamentos básicos de programación de Smart Contracts con Solidity/1_primeros_pasos.sol
+++ 10 - Fundamentos básicos de programación de Smart Contracts con Solidity/1_primeros_pasos.sol
@@ -4,7 +4,7 @@
 pragma solidity ^0.8.4; // o pragma solidity >=0.6.0 <0.8.0;
 
 // Importar un contrato inteligente desde Openzeppelin
-import "@openzeppelin/contracts@4.4.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
 
 // Declaracion del Smart Contract
 contract FirstContract is ERC721 {
