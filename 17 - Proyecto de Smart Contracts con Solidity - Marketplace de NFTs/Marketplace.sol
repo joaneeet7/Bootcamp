8613ec44c690cce68f9222a656a599157ec5c0bd
Joan Amengual <joan@consilientlabs.com>
2022-04-22 15:36:18 -0700

    Update Marketplace.sol

--- 17 - Proyecto de Smart Contracts con Solidity - Marketplace de NFTs/Marketplace.sol
+++ 17 - Proyecto de Smart Contracts con Solidity - Marketplace de NFTs/Marketplace.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 import "hardhat/console.sol";
 
 contract Marketplace is ReentrancyGuard {
