a98e049c43019d98b715758198b6752d0e02919e
Joan Amengual <joan@consilientlabs.com>
2022-05-14 16:48:54 +0200

    Update

--- 10-Fundamentos básicos de programación de Smart Contracts con Solidity/5_payable.sol
+++ 10-Fundamentos básicos de programación de Smart Contracts con Solidity/5_payable.sol
@@ -1,8 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract functionPayable {
+contract ethSend {
 
+    constructor() payable {}
+    receive() external payable {}
 
+    // Eventos 
+    event sendStatus(bool);
+    event callStatus(bool, bytes);
+
+    // Transfer
+    function sendViaTransfer(address payable _to) public payable {
+        _to.transfer(1 ether);
+    }
+
+    // Send
+    function sendViaSend(address payable _to) public payable {
+        bool sent = _to.send(1 ether);
+        emit sendStatus(sent);
+        require(sent == true, "El envio ha fallado");
+    }
+
+    // Call
+    function sendViaCall(address payable _to) public payable {
+        (bool success, bytes memory data) = _to.call{value: 1 ether}("");
+        emit callStatus(success, data);
+        require(success == true, "El envio ha fallado");
+    }
 
 }
+
+
+contract ethReceiver {
+
+    event log(uint amount, uint gas);
+
+    receive() external payable {
+        emit log(address(this).balance, gasleft());
+    }
+
+} 
---
+++ 10-Fundamentos básicos de programación de Smart Contracts con Solidity/6_fallback_receive.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/* ¿fallback() y receive()?
+
+msg.data -> El calldata completo, que es un area no modificable y no persistente 
+            en la que se almacenan los argumentos de la funcion y que se comporta 
+            principalmente como la memoria.
+
+    Ether enviado al Smart Contract
+                    |
+              ¿msg.data vacio?
+                /            \
+               si            no
+               /               \
+       existe receive()?     fallback()
+           /       \
+          si       no
+          /          \
+      receive()   fallback()
+*/
+
+contract Fallback_Receive{
+
+    event log (string, address, uint, bytes);
+    
+    fallback() external payable {
+        emit log("fallback", msg.sender, msg.value, msg.data);
+    }
+
+    receive() external payable {
+        emit log("receive", msg.sender, msg.value, "");
+    }
+}
