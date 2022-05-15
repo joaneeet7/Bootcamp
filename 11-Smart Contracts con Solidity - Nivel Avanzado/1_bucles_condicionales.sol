ccd7a525d7885f0d5ec4aa22173c11ec2f1aaf49
Joan Amengual <joan@consilientlabs.com>
2022-05-15 11:31:19 +0200

    Update 1_bucles_condicionales.sol

--- 11-Smart Contracts con Solidity - Nivel Avanzado/1_bucles_condicionales.sol
+++ 11-Smart Contracts con Solidity - Nivel Avanzado/1_bucles_condicionales.sol
@@ -2,27 +2,58 @@
 pragma solidity ^0.8.4;
 
 contract loops_conditionals {
-    
-    // Suma de los 10 primeros numeros a partir del numero introducido
-    function sum (uint256 _number) public pure returns (uint256){
-        uint256 aux_sum = 0;
-        for (uint256 i = _number; i < (10+_number); i++){
+
+    // Suma de los 10 primeros numeros a partir de un numero introducido 
+    function sum(uint _number) public pure returns (uint){
+        uint aux_sum = 0;
+
+        // Bucle for 
+        for (uint i = _number; i < (10 + _number); i++){
             aux_sum = aux_sum + i;
         }
+
         return aux_sum;
     }
 
     // Suma de los 10 primeros numeros impares 
-    function odd() public pure returns (uint256){
-        uint256 aux_sum = 0;
-        uint256 counter = 1;
-        while (counter < 10) {
-            if(counter % 2 != 0){
+    function odd() public pure returns (uint){
+        uint aux_sum = 0;
+        uint counter = 0;
+        uint counter_odd = 0;
+
+        while(counter_odd < 10){
+            // Par -> a % 2 = 0
+            // Impar -> a % 2 != 0
+            if (counter % 2 != 0){
                 aux_sum = aux_sum + counter;
+                counter_odd++;
             }
             counter++;
         }
+
         return aux_sum;
     }
 
+    // Operaciones matematicas
+    function operations(string memory operation, uint a, uint b) public pure returns (uint){
+        
+        // Hash de la operacion
+        bytes32 hash_operation = keccak256(abi.encodePacked(operation));
+
+        if (hash_operation == keccak256(abi.encodePacked("suma"))){
+            return a+b;
+        } 
+        else if (hash_operation == keccak256(abi.encodePacked("resta"))){
+            return a-b;
+        }
+        else if (hash_operation == keccak256(abi.encodePacked("division"))){
+            return a/b;
+        }
+        else if (hash_operation == keccak256(abi.encodePacked("multiplicacion"))){
+            return a*b;
+        }        
+        else {
+            return 0;
+        }
+    }
 }
