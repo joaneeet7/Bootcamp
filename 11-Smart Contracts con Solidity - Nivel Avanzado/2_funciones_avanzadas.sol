649d11528de776b79d161d7b39f36939e77d7231
Joan Amengual <joan@consilientlabs.com>
2022-05-15 11:50:20 +0200

    Update 2_funciones_avanzadas.sol

--- 11-Smart Contracts con Solidity - Nivel Avanzado/2_funciones_avanzadas.sol
+++ 11-Smart Contracts con Solidity - Nivel Avanzado/2_funciones_avanzadas.sol
@@ -2,22 +2,25 @@
 pragma solidity ^0.8.4;
 
 contract Food {
-    
+
     // Estructura de datos 
     struct dinnerPlate {
         string name;
         string ingredients;
     }
-    // Menu del dia
-    dinnerPlate [] menu;
 
-    // Creacion de un nuevo menu 
-    function newPlate (string memory _name, string memory _ingredients) internal {
+    // Menu del dia
+    dinnerPlate [] private menu;
+
+    // Creacion de un nuevo menu
+    function newMenu(string memory _name, string memory _ingredients) internal {
         menu.push(dinnerPlate(_name, _ingredients));
     }
 }
 
-contract Hamburger is Food {
+
+contract Hamburguer is Food {
+
     address public owner;
 
     constructor () {
@@ -25,20 +28,19 @@ contract Hamburger is Food {
     }
 
     // Cocinar una hamburguesa desde el Smart Contract principal
-    function doHamburguer(string memory _ingredients, uint256 _units) external {
-        require(_units <= 5, "Ups, please check the units of hamburgers!");
-        newPlate("Hamburger", _ingredients);
+    function doHamburguer(string memory _ingredients, uint _units) external {
+        require (_units <= 5, "Ups, no puedes pedir tantas hamburguesas");
+        newMenu("Hamburguer", _ingredients);
     }
 
-    // Funcion restringida al owner del Smart Contract
-    function hashPrivateNumber(uint256 _number) public view onlyOwner returns (bytes32){
-        return keccak256(abi.encodePacked(_number));
-    }
-
-    // Modifier para permitir el acceso al owner y solo al owner
+    // Modifier: permitir el acceso unicamente al owner y solo al owner
     modifier onlyOwner() {
-        require(owner == msg.sender,"You do not have permissions to execute this function");
+        require(owner == msg.sender, "No tienes permisos para ejecutar esta funcion");
         _;
     }
 
+    // Restriccion a la ejecucion (solo puede el owner)
+    function hashPrivateNumber(uint _number) public view onlyOwner returns (bytes32){
+        return keccak256(abi.encodePacked(_number));
+    }
 }
