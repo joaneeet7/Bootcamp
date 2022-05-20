f6cbbbb8e43b1aa411ceb0f704d33e2ee8b73bf2
Joan Amengual <joan@consilientlabs.com>
2022-05-20 02:50:55 +0200

    Update

--- 29-DApp con React - DeFi (Decentralized Finance)/src/contracts/TokenFarm.sol
+++ 29-DApp con React - DeFi (Decentralized Finance)/src/contracts/TokenFarm.sol
@@ -1,79 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./StellartToken.sol";
 import "./JamToken.sol";
+import "./StellartToken.sol";
 
 contract TokenFarm {
 
     // Declaraciones iniciales
     string public name = "Stellart Token Farm";
     address public owner;
-    StellartToken public stellartToken;
     JamToken public jamToken;
+    StellartToken public stellartToken;
 
-    // Estructuras de datos
-    address[] public stakers;
+    // Estructuras de datos 
+    address [] public stakers;
     mapping(address => uint) public stakingBalance;
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaking;
 
-    // Constructor
+    // Constructor 
     constructor(StellartToken _stellartToken, JamToken _jamToken) {
         stellartToken = _stellartToken;
         jamToken = _jamToken;
         owner = msg.sender;
     }
 
-    // Hacer stake de tokens
+    // Stake de tokens 
     function stakeTokens(uint _amount) public {
-        // Se requiere una cantidad superior a 0
-        require(_amount > 0, "La cantidad no puede ser 0");
-
-        //  Transferir tokens JAM Tokens a este contrato para staking
+        // Se require una cantidad superior a 0 
+        require(_amount > 0, "La cantidad no puede ser menor a 0");
+        // Transferir tokens JAM al Smart Contract principal
         jamToken.transferFrom(msg.sender, address(this), _amount);
-
         // Actualizar el saldo del staking
-        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
-
-        // Añadir el usuario al array de stakers *solo* si no han hecho staking antes
-        if(!hasStaked[msg.sender]) {
+        stakingBalance[msg.sender] += _amount;
+        // Guardar el staker
+        if(!hasStaked[msg.sender]){
             stakers.push(msg.sender);
         }
-
         // Actualizar el estado del staking
         isStaking[msg.sender] = true;
         hasStaked[msg.sender] = true;
     }
 
-    // Quitar el staking de los tokens (Withdraw)
+    // Quitar el staking de los tokens 
     function unstakeTokens() public {
-        // Saldo del staking
+        // Saldo del staking de un usuario 
         uint balance = stakingBalance[msg.sender];
-
-        // Se requiere una cantidad superior a 0
-        require(balance > 0, "El balance de staking no puede ser 0");
-
-        // Transferir tokens de JAM Tokens a este contrato para staking
+        // Se require una cantidad superior a 0
+        require(balance > 0, "El balance del staking es 0");
+        // Transferencia de los tokens al usuario
         jamToken.transfer(msg.sender, balance);
-
-        // Resetear el balance staking
+        // Resetea el balance de staking del usuario 
         stakingBalance[msg.sender] = 0;
-
         // Actualizar el estado del staking
         isStaking[msg.sender] = false;
     }
 
-    // Emision de Tokens
+    // Emision de Tokens (recompensas)
     function issueTokens() public {
-        // Solo el propietario puede llamar a esta funcion
-        require(msg.sender == owner, "No eres el Owner del Smart Contract");
-
+        // Unicamente ejecutable por el owner
+        require(msg.sender == owner, "No eres el owner");
         // Emitir tokens a todos los stakers
-        for (uint i=0; i<stakers.length; i++) {
+        for (uint i=0; i < stakers.length; i++){
             address recipient = stakers[i];
             uint balance = stakingBalance[recipient];
-            if(balance > 0) {
+            if(balance > 0){
                 stellartToken.transfer(recipient, balance);
             }
         }
