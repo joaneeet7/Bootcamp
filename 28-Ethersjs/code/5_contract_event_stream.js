1062081e188ae8c6e2f406d1fbb329afd0b1717f
Joan Amengual <joan@consilientlabs.com>
2022-05-25 15:20:45 +0200

    Update

--- 28-Ethersjs/code/5_contract_event_stream.js
+++ 28-Ethersjs/code/5_contract_event_stream.js
@@ -1,22 +1,23 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
+const INFURA_ID = '7c9cd8b7cd234cbdbcc5f2ab4b55a514'
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
 const ERC20_ABI = [
     "function name() view returns (string)",
     "function symbol() view returns (string)",
     "function totalSupply() view returns (uint256)",
-    "function balanceOf(address) view returns (uint)",
-
-    "event Transfer(address indexed from, address indexed to, uint amount)"
+    "function balanceOf(address) view returns (uint256)",
+    "event Transfer(address indexed from, address indexed to, uint amount)",
 ];
 
 const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
 const contract = new ethers.Contract(address, ERC20_ABI, provider)
 
 const main = async () => {
+
     const block = await provider.getBlockNumber()
+    console.log(block)
 
     const transferEvents = await contract.queryFilter('Transfer', block - 1, block)
     console.log(transferEvents)
--- 28-Ethersjs/code/6_inspecting_blocks.js
+++ 28-Ethersjs/code/6_inspecting_blocks.js
@@ -1,20 +1,17 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
+const INFURA_ID = '7c9cd8b7cd234cbdbcc5f2ab4b55a514'
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
 const main = async () => {
-    const block = await provider.getBlockNumber()
 
-    console.log(`\nBlock Number: ${block}\n`)
+    const block = await provider.getBlockNumber()
+    console.log(`\nBlock number: ${block}`)
 
     const blockInfo = await provider.getBlock(block)
-
     console.log(blockInfo)
 
-    const { transactions } = await provider.getBlockWithTransactions(block)
-
-    console.log(`\nLogging first transaction in block:\n`)
+    const {transactions} = await provider.getBlockWithTransactions(block)
     console.log(transactions[0])
 }
 
